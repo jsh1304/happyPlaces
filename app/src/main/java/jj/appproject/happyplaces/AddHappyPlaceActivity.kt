2c3a69da66200f88ae76c30663a573ca1a5fac1a
jsh1304 <jung3914a@gmail.com>
2022-04-05 21:43:17 +0900

    220405_app setting

---
+++ app/src/main/java/jj/appproject/happyplaces/AddHappyPlaceActivity.kt
@@ -0,0 +1,13 @@
+package jj.appproject.happyplaces
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class AddHappyPlaceActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_add_happy_place)
+    }
+}
