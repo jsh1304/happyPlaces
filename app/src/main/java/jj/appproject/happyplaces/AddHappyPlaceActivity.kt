25c1798813c153290ff0ddcd03a80bae97a7c37e
jsh1304 <jung3914a@gmail.com>
2022-04-06 21:41:49 +0900

    220406_AddHappyPlaceActivity

--- app/src/main/java/jj/appproject/happyplaces/AddHappyPlaceActivity.kt
+++ app/src/main/java/jj/appproject/happyplaces/AddHappyPlaceActivity.kt
@@ -1,13 +1,21 @@
 package jj.appproject.happyplaces
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import jj.appproject.happyplaces.databinding.ActivityAddHappyPlaceBinding
 
-class AddHappyPlaceActivity: AppCompatActivity() {
+class AddHappyPlaceActivity : AppCompatActivity() {
+    private var binding: ActivityAddHappyPlaceBinding? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(binding?.root)
 
-        setContentView(R.layout.activity_add_happy_place)
+        setSupportActionBar(binding?.toolbarAddPlace)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        binding?.toolbarAddPlace?.setNavigationOnClickListener {
+            onBackPressed()
+        }
     }
 }
--- app/src/main/java/jj/appproject/happyplaces/MainActivity.kt
+++ app/src/main/java/jj/appproject/happyplaces/MainActivity.kt
@@ -1,11 +1,27 @@
 package jj.appproject.happyplaces
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import jj.appproject.happyplaces.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var binding: ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        binding?.flAdd?.setOnClickListener {
+            val intent = Intent(this, AddHappyPlaceActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+
+    override fun onDestroy(){ // memory 누락 방지 위해 binding을 해제하는 방법
+        super.onDestroy()
+        binding = null
     }
 }
